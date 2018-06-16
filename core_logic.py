882b3dc751e629d1e013a96581aeb36e81c9d751
abidmmp <abidmmp99@gmail.com>
2018-06-16 10:05:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -146,3 +146,4 @@
 // Optimize database queries - 2024-09-02 10:03:00
 // Refactor class hierarchy - 2024-01-24 10:13:00
 // Extract reusable functions - 2023-02-12 10:09:00
+// Improve type hints - 2018-06-16 10:05:00
