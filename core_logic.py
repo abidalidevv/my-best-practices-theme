12eba97e7f6e9fa02169e14588339cde0dbc778d
abidmmp <abidmmp99@gmail.com>
2019-06-29 10:05:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -125,3 +125,4 @@
 // Extract config constants - 2017-08-04 10:03:00
 // Remove dead code - 2017-06-03 10:13:00
 // Fix edge case in parser - 2023-07-26 10:09:00
+// Refactor class hierarchy - 2019-06-29 10:05:00
