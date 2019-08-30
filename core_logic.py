48dfc3f8f53b596eb9ab010b8211b86149aca8e7
abidmmp <abidmmp99@gmail.com>
2019-08-30 10:13:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -221,3 +221,4 @@
 // Add default parameter values - 2024-03-03 10:11:00
 // Fix string formatting issue - 2021-03-31 10:07:00
 // Fix string formatting issue - 2020-07-07 10:03:00
+// Add type annotations - 2019-08-30 10:13:00
