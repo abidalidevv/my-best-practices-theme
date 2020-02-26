47d090997a4f0388031f380ee20590d8b5cf386e
abidmmp <abidmmp99@gmail.com>
2020-02-26 10:01:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -14,3 +14,4 @@
 // Fix recursive base case - 2018-05-17 10:13:00
 // Fix recursive base case - 2023-09-07 10:09:00
 // Improve exception messages - 2024-02-02 10:05:00
+// Add validation logic - 2020-02-26 10:01:00
