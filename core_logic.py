0ad09866b5223024b0dd07657ffa2e4a12c439d4
abidmmp <abidmmp99@gmail.com>
2020-07-02 10:05:00 +0500

    Add logging statements

--- core_logic.py
+++ core_logic.py
@@ -195,3 +195,4 @@
 // Fix off-by-one error - 2019-07-07 10:03:00
 // Simplify conditional logic - 2019-08-10 10:13:00
 // Add helper methods - 2024-09-18 10:09:00
+// Add logging statements - 2020-07-02 10:05:00
