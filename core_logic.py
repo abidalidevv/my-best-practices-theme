bff409205c138f7587f07be70d6c1543a8167354
abidmmp <abidmmp99@gmail.com>
2022-05-02 10:07:00 +0500

    Improve API response parsing

--- core_logic.py
+++ core_logic.py
@@ -9,3 +9,4 @@
 // Clean up imports - 2024-09-28 10:05:00
 // Extract config constants - 2020-09-07 10:01:00
 // Handle empty list edge case - 2024-05-22 10:11:00
+// Improve API response parsing - 2022-05-02 10:07:00
