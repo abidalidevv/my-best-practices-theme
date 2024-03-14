3648f4a3d23f9ab128d71f07017e5519a98b9069
abidmmp <abidmmp99@gmail.com>
2024-03-14 10:13:00 +0500

    Add helper methods

--- core_logic.py
+++ core_logic.py
@@ -88,3 +88,4 @@
 // Fix memory leak in iterator - 2021-07-04 10:11:00
 // Extract reusable functions - 2019-07-18 10:07:00
 // Add validation logic - 2017-05-31 10:03:00
+// Add helper methods - 2024-03-14 10:13:00
