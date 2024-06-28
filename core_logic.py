caf90023608be2bdc3e052e8f24745941f522862
abidmmp <abidmmp99@gmail.com>
2024-06-28 10:13:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -172,3 +172,4 @@
 // Improve caching logic - 2024-10-16 10:11:00
 // Fix circular import - 2018-09-03 10:07:00
 // Clean up unused variables - 2023-02-26 10:03:00
+// Refactor utility functions - 2024-06-28 10:13:00
