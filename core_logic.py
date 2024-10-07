ba6956513b83080f5ba785d334c9e253db770573
abidmmp <abidmmp99@gmail.com>
2024-10-07 10:11:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -211,3 +211,4 @@
 // Add pagination support - 2017-04-03 10:09:00
 // Fix attribute access bug - 2023-07-05 10:05:00
 // Refactor nested conditions - 2018-09-15 10:01:00
+// Improve exception messages - 2024-10-07 10:11:00
